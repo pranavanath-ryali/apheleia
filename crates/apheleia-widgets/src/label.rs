1aabe33aa5304a8d3cc5d99784387da644f43a51
Nitrostation-Dev <dev.nitrostation@gmail.com>
2025-12-12 01:04:09 +0530

    only define the size of Label if not defined

--- crates/apheleia-widgets/src/label.rs
+++ crates/apheleia-widgets/src/label.rs
@@ -1,22 +1,32 @@
 use apheleia_core::{buffer::NodeBuffer, style::Style, types::vector::Vector2};
-use apheleia_ui::node::{data::NodeData, node::NodeTrait};
+use apheleia_ui::node::{
+    data::{self, NodeData},
+    node::NodeTrait,
+};
 
 pub struct Label {
-    text: String
+    text: String,
 }
 
 impl Label {
     pub fn new(text: &str) -> Self {
-        Label { text: text.to_string() }
+        Label {
+            text: text.to_string(),
+        }
     }
 }
 
 impl NodeTrait for Label {
     fn initial_setup(&mut self, data: &mut NodeData) {
-        data.size = Some(Vector2(self.text.len() as u16, 1));
+        match data.size {
+            None => {
+                data.size = Some(Vector2(self.text.len() as u16, 1));
+            },
+            _ => {}
+        }
     }
 
-    fn update(&mut self) {    }
+    fn update(&mut self) {}
     fn render(&self, buf: &mut NodeBuffer) {
         buf.write_line(0, 0, &self.text, Some(Style::default()));
     }
