ac0f4e63c0b00d1cd629695ab97e698254420f45
Nitrostation-Dev <dev.nitrostation@gmail.com>
2025-12-15 11:34:14 +0530

    render block widgets

--- crates/apheleia-widgets/src/block.rs
+++ crates/apheleia-widgets/src/block.rs
@@ -1,4 +1,5 @@
-use apheleia_ui::{commands::{self, InitialCallContext}, node::node::NodeTrait};
+use apheleia_core::{Color, style::Style};
+use apheleia_ui::{commands::{self, InitialCallContext}, contexts::RenderContext, node::node::NodeTrait};
 
 pub struct Block;
 impl NodeTrait for Block {
@@ -7,10 +8,15 @@ impl NodeTrait for Block {
     fn event(&mut self) {}
     fn update(&mut self) {}
 
-    fn render(&self, buf: &mut apheleia_core::buffer::Buffer) {
+    fn render(&self, ctx: &mut RenderContext, buf: &mut apheleia_core::buffer::Buffer) {
         buf.write_line(0, 0, "+", None);
-        // buf.write_line(0, self., "+", None);
-        // buf.write_line(0, 0, "+", None);
-        // buf.write_line(0, 0, "+", None);
+        buf.write_line(0, ctx.size.1, "+", None);
+        buf.write_line(ctx.size.0, 0, "+", None);
+        buf.write_line(ctx.size.0, ctx.size.1, "+", None);
+
+        buf.write_line(1, 1, &format!("X: {}", ctx.position.0), Some(Style { fg: Color::Blue, ..Default::default() }));
+        buf.write_line(1, 2, &format!("Y: {}", ctx.position.1), Some(Style { fg: Color::Blue, ..Default::default() }));
+        buf.write_line(1, 3, &format!("WIDTH: {}", ctx.size.0), Some(Style { fg: Color::Red, ..Default::default() }));
+        buf.write_line(1, 4, &format!("HEIGHT: {}", ctx.size.1), Some(Style { fg: Color::Red, ..Default::default() }));
     }
 }
