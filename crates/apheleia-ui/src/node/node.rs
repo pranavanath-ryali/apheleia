e58b5f97b60667aa25f985d6dd6d0b2846e97828
Nitrostation-Dev <dev.nitrostation@gmail.com>
2026-01-05 23:27:50 +0530

    fixed minor issue

--- crates/apheleia-ui/src/node/node.rs
+++ crates/apheleia-ui/src/node/node.rs
@@ -1,11 +1,11 @@
 use apheleia_core::buffer::Buffer;
 
-use crate::contexts::{EventContext, InitialCallContext, RenderContext, UpdateContext};
+use crate::contexts::{EventUpdateContext, InitialCallContext, RenderContext, UpdateContext};
 
 pub trait NodeTrait {
     fn initial_setup(&mut self, ctx: &mut InitialCallContext);
 
-    fn event(&mut self, ctx: &EventContext);
+    fn event(&mut self, ctx: &EventUpdateContext);
     fn update(&mut self, ctx: &mut UpdateContext);
     fn render(&self, ctx: &mut RenderContext, buf: &mut Buffer);
 }
