8ba25d059d4affa3ab1dd938bcfd8a70eca4bb5a
pranavanath-ryali <dev.nitrostation@gmail.com>
2026-03-19 20:38:10 +0530

    moved emptynode to node module mod.rs

--- crates/apheleia-ui/src/lib.rs
+++ crates/apheleia-ui/src/lib.rs
@@ -10,13 +10,3 @@ pub const FAKE_NODEID: NodeId = 0;
 pub const MAX_NODES: NodeId = 1000;
 
 pub use crossterm::event::*;
-
-use crate::node::node::NodeTrait;
-
-pub struct EmptyNode;
-impl NodeTrait for EmptyNode {
-    fn initial_setup(&mut self, _ctx: &mut contexts::Context) {}
-    fn event(&mut self, _ctx: &mut contexts::Context) {}
-    fn update(&mut self, _ctx: &mut contexts::Context) {}
-    fn render(&self, _buf: &mut apheleia_core::buffer::Buffer, _ctx: &mut contexts::Context) {}
-}
--- crates/apheleia-ui/src/node/mod.rs
+++ crates/apheleia-ui/src/node/mod.rs
@@ -1,2 +1,12 @@
+use crate::{contexts, node::node::NodeTrait};
+
 pub mod data;
 pub mod node;
+
+pub struct EmptyNode;
+impl NodeTrait for EmptyNode {
+    fn initial_setup(&mut self, _ctx: &mut contexts::Context) {}
+    fn event(&mut self, _ctx: &mut contexts::Context) {}
+    fn update(&mut self, _ctx: &mut contexts::Context) {}
+    fn render(&self, _buf: &mut apheleia_core::buffer::Buffer, _ctx: &mut contexts::Context) {}
+}
