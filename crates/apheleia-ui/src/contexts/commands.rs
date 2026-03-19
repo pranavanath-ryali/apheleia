00ab46a03dfe9280a98bd86dc22416cdb413a8ee
pranavanath-ryali <dev.nitrostation@gmail.com>
2026-03-19 23:08:29 +0530

    bare minimum commands for now

--- crates/apheleia-ui/src/contexts/commands.rs
+++ crates/apheleia-ui/src/contexts/commands.rs
@@ -6,64 +6,35 @@ use crate::{
     types::{DirtyRenderLevel, EventType, NodeId, UpdateTypeNode},
 };
 
-pub struct SetSizeForId(pub NodeId, pub Vector2);
-pub struct SetPositionForId(pub NodeId, pub Vector2);
+pub struct SetSize(pub Vector2);
+pub struct SetSizeForNode(pub String, pub Vector2);
+
 pub struct RegisterForUpdate;
 pub struct RegisterForEvent(pub EventType);
 
 pub struct MarkRenderDirty(pub NodeId, pub DirtyRenderLevel);
 pub struct MarkUpdateDirty(pub NodeId);
 
-impl ContextCommand for SetSizeForId {
-    fn execute(self: Box<Self>, _id: NodeId, rootnode_data: &mut RootNodeData) {
+impl ContextCommand for SetSize {
+    fn execute(self: Box<Self>, id: NodeId, rootnode_data: &mut RootNodeData) {
         rootnode_data
             .node_storage
             .borrow_mut()
-            .get_data_mut(self.0)
+            .get_data_mut(id)
             .unwrap()
-            .set_size(self.1);
+            .set_size(self.0);
     }
 }
-impl ContextCommand for SetPositionForId {
-    fn execute(self: Box<Self>, id: NodeId, rootnode_data: &mut RootNodeData) {
-        // TODO
-        rootnode_data
-            .node_storage
-            .borrow_mut()
-            .get_data_mut(self.0)
-            .unwrap()
-            .set_position(self.1);
-
-        let mut position = rootnode_data
-            .node_storage
-            .borrow()
-            .get_data(id)
-            .unwrap()
-            .position;
-
-        rootnode_data
-            .relations
-            .get_ancestor_ids(&id)
-            .unwrap()
-            .iter()
-            .filter(|id| **id != 0_usize)
-            .for_each(|node_id| {
-                let pos = rootnode_data
-                    .node_storage
-                    .borrow()
-                    .get_data(*node_id)
-                    .unwrap()
-                    .position;
-                position.0 += pos.0;
-                position.1 += pos.1;
-            });
-
-        rootnode_data
-            .node_storage
-            .borrow_mut()
-            .get_data_mut(self.0)
-            .unwrap()
-            .set_global_position(position);
+impl ContextCommand for SetSizeForNode {
+    fn execute(self: Box<Self>, _id: NodeId, rootnode_data: &mut RootNodeData) {
+        if let Some(id) = rootnode_data.node_storage.borrow().get_id(self.0.as_str()) {
+            rootnode_data
+                .node_storage
+                .borrow_mut()
+                .get_data_mut(*id)
+                .unwrap()
+                .set_size(self.1);
+        }
     }
 }
 impl ContextCommand for RegisterForUpdate {
