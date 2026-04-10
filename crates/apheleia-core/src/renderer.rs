45e651bd8ea3e89b4ebc8ad4b8ddc5020a021c52
pranavanath-ryali <dev.nitrostation@gmail.com>
2026-04-10 17:33:51 +0530

    moved single file directories to their own files

---
+++ crates/apheleia-core/src/renderer.rs
@@ -0,0 +1,166 @@
+use std::io::{Error, Stdout, Write, stdout};
+
+use crossterm::{
+    cursor::{self, MoveTo},
+    execute, queue,
+    style::{
+        Attribute, Attributes, Print, SetAttribute, SetAttributes, SetBackgroundColor,
+        SetForegroundColor,
+    },
+    terminal::{Clear, disable_raw_mode, enable_raw_mode},
+};
+
+use crate::{
+    buffer::Buffer,
+    style::{Style, StyleFlags},
+};
+
+pub struct Renderer {
+    pub size: (u16, u16),
+    pub stdout: Stdout,
+}
+impl Renderer {
+    pub fn new(width: u16, height: u16) -> Self {
+        Self {
+            size: (width, height),
+            stdout: stdout(),
+        }
+    }
+
+    pub fn init(&mut self) {
+        _ = enable_raw_mode();
+        _ = execute!(self.stdout, cursor::Hide);
+        _ = execute!(self.stdout, Clear(crossterm::terminal::ClearType::All));
+    }
+
+    pub fn render_flip(&mut self, buf: &mut Buffer) {
+        _ = execute!(self.stdout, Clear(crossterm::terminal::ClearType::All));
+
+        for y in 0..self.size.1 {
+            let mut batch_text = String::new();
+            let mut style = Style::default();
+            let mut start_x = 0u16;
+
+            for x in 0..self.size.0 {
+                let cell = buf.get_cell(x, y);
+
+                if cell.style == style {
+                    batch_text.push(cell.c);
+                    continue;
+                }
+
+                _ = queue_batch(&mut self.stdout, start_x, y, &batch_text, style);
+
+                batch_text.clear();
+                batch_text.push(cell.c);
+                style = cell.style;
+                start_x = x;
+            }
+
+            _ = queue_batch(&mut self.stdout, start_x, y, &batch_text, style);
+        }
+
+        _ = self.stdout.flush();
+        buf.clear_diff();
+    }
+
+    pub fn render(&mut self, buf: &mut Buffer) {
+        for (y, map) in buf.get_diffed_cells().iter() {
+            let mut batch_text = String::new();
+            let mut style = Style::default();
+            let mut start_x = 0u16;
+            let mut offset = 0u16;
+
+            for (x, cell) in map.iter() {
+                if *x != start_x + offset + 1 {
+                    _ = queue_batch(&mut self.stdout, start_x, *y, &batch_text, style);
+
+                    start_x = *x;
+                    offset = 0;
+                    style = cell.style;
+                    batch_text.clear();
+                    batch_text.push(cell.c);
+
+                    continue;
+                }
+
+                if cell.style != style {
+                    _ = queue_batch(&mut self.stdout, start_x, *y, &batch_text, style);
+
+                    start_x = *x;
+                    offset = 0;
+                    style = cell.style;
+                    batch_text.clear();
+                    batch_text.push(cell.c);
+                }
+
+                offset += 1;
+                batch_text.push(cell.c);
+            }
+
+            _ = queue_batch(&mut self.stdout, start_x, *y, &batch_text, style);
+        }
+
+        _ = self.stdout.flush();
+    }
+
+    pub fn quit(&mut self) {
+        _ = execute!(self.stdout, cursor::Show);
+        _ = disable_raw_mode();
+    }
+}
+
+fn queue_batch(
+    stdout: &mut Stdout,
+    x: u16,
+    y: u16,
+    text: &String,
+    style: Style,
+) -> Result<(), Error> {
+    queue!(stdout, SetAttribute(Attribute::Reset))?;
+
+    queue!(stdout, MoveTo(x, y))?;
+    queue!(stdout, SetForegroundColor(style.fg))?;
+    queue!(stdout, SetBackgroundColor(style.bg))?;
+    queue_flags(stdout, style.flags)?;
+    queue!(stdout, Print(text.to_string()))?;
+
+    Ok(())
+}
+
+fn queue_flags(stdout: &mut Stdout, flags: StyleFlags) -> Result<(), Error> {
+    let mut attr = Attributes::default();
+
+    if flags.contains(StyleFlags::BOLD) {
+        attr.set(Attribute::Bold);
+    }
+    if flags.contains(StyleFlags::ITALIC) {
+        attr.set(Attribute::Italic);
+    }
+    if flags.contains(StyleFlags::DIM) {
+        attr.set(Attribute::Dim);
+    }
+    if flags.contains(StyleFlags::REVERSE) {
+        attr.set(Attribute::Reverse);
+    }
+    if flags.contains(StyleFlags::UNDER_CURLED) {
+        attr.set(Attribute::Undercurled);
+    }
+    if flags.contains(StyleFlags::UNDER_LINED) {
+        attr.set(Attribute::Underlined);
+    }
+    if flags.contains(StyleFlags::UNDER_DOTTED) {
+        attr.set(Attribute::Underdotted);
+    }
+    if flags.contains(StyleFlags::UNDER_DASHED) {
+        attr.set(Attribute::Underdashed);
+    }
+    if flags.contains(StyleFlags::DOUBLE_UNDERLINED) {
+        attr.set(Attribute::DoubleUnderlined);
+    }
+    if flags.contains(StyleFlags::SLOW_BLINK) {
+        attr.set(Attribute::SlowBlink);
+    }
+
+    queue!(stdout, SetAttributes(attr))
+}
